0fe4c6a81294af48f5667e3b7884799b573c9e36
MirruK <mirek.kudinoff@abo.fi>
2023-01-15 22:39:09 +0200

    Added code to serve frontend

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ morgan.token('postRes', (req, res)=>{
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms  POST :postRes'))
 app.get('/info', (req, res)=>{
     console.log("GET request for info inbound")
