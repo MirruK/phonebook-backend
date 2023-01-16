5716c92cda31b2b24b4529cc32bf00d09828a264
MirruK <mirek.kudinoff@abo.fi>
2023-01-16 23:17:45 +0200

    MongoDB integration

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./mongo').Person
+const mongoose = require('mongoose')
+const ObjectId = require('mongoose').Types.ObjectId;
 
-const persons = {
-    persons:[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]}
+const URI = process.env.MONGODB_URI
+mongoose
+    .connect(URI)
+    .then(()=>{
+        console.log("Connected")
+    })
+    .catch((err)=>console.log(err))
 
 const app = express()
-const PORT = 3001
 
 const getNewId = () => Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)
 
@@ -52,50 +39,74 @@ app.get('/info', (req, res)=>{
 
 app.get('/api/persons', (req, res)=>{
     console.log("GET request for persons inbound")
-    res.json(persons)
+    Person.find({}).then((persons)=>res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res)=>{
-    const reqId = Number(req.params.id)
-    console.log("GET request for a person inbound")
-    const requestPerson = persons.persons.find((person)=>person.id === reqId)
-    if (!requestPerson){
-        res.status(400)
-        res.send("No person found for the given id")
-    } 
-    else res.send(requestPerson)
+    if(!ObjectId.isValid(req.params.id)){
+        res.sendStatus(400).end()   
+        return
+    }
+    Person.findById(req.params.id)
+    .then(pers => {
+      if (pers){
+          res.json(pers)
+          res.send(200).end()
+          return
+      }else{
+          res.status(404).end()
+      }}
+    )
+    .catch((err)=>{console.log("No person found with the requested id ", err); return})
 })
 
 app.delete('/api/persons/:id', (req, res)=>{
-    const reqId = Number(req.params.id)
+    if(!ObjectId.isValid(req.params.id)){
+        res.sendStatus(400).end()   
+        return
+    }
+    Person.findOneAndDelete({_id : req.params.id})
+    .catch(err => {
+         console.log("Deletion failed. Error: ", err) 
+      })
     console.log("DELETE request for a person inbound")
-    persons.persons = persons.persons.filter((person)=>person.id !== reqId)
     res.sendStatus(204).end()
 })
 
 app.post('/api/persons', (req, res)=>{
-    const newId = getNewId()
-    console.log("Object keys: ",Object.keys(req.body))
-    if(checkNewPerson(req.body)){
-        const newPerson = {name : req.body.name, number: req.body.number, id:newId}
-        console.log("POST request to persons inbound")
-        if(persons.persons.find((person)=>person.name === newPerson.name)){
-            res.status(400)
-            res.send(`A person with the name ${newPerson.name} already exists`)
-            return
-        }
-        persons.persons = persons.persons.concat(newPerson)
-        res.status(201)
-        res.send("Created new person")
-    }
-    else {
+    //LINES .,+8 REFACTOR TO MONGO
+    if(!checkNewPerson(req.body)){
         res.status(400)
-        res.send("Please provide both a name and number for your new person")
+        res.send("Please provide both a name and number for your new person").end()
+        return;
     }
+    console.log("POST request to persons inbound")
+    Person.exists(req.body, (err)=>{
+        if(err){
+            res.status(400)
+            res.send("Person with these credentials already exists. Use a PUT request to update").end()
+            return;
+        }
+        else{
+            const newPerson = new Person({name : req.body.name, number: req.body.number})
+            newPerson.save((err, savedPerson)=> {
+                if (err) return;
+                else{
+                    res.status(201)
+                    res.send({statusMessage : "Created new person", newPerson: savedPerson}).end()
+                    return;
+                }
+            })
+        }
+    }) 
 })
+
 const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(server_port, server_host, function() {
     console.log('Listening on port', server_port);
 });
-
+process.on ("SIGINT", async() => {
+  await mongoose.connection.close ();
+  process.exit (0)
+});
